af3531b238858fb1ec9379a03ddd0b839d480533
AriaCundick <aria.cundick@gmail.com>
2016-04-16 15:56:43 -0500

    c

---
+++ namesController.js
@@ -0,0 +1,14 @@
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+	$scope.items = [
+		{name: "Peanut Butter", quantity: 24, price: 4.29},
+		{name: "Jelly", quantity: 14, price: 3.29},
+		{name: "Pickles", quantity: 54, price: 2.50},
+		{name: "Bananas", quantity: 7, price: 2.10},
+		{name: "Apples", quantity: 32, price: 1.50},
+		{name: "Peaches", quantity: 19, price: 1.25},
+		{name: "Ice cream", quantity: 36, price: 1.89},
+	];
+	$scope.orderByMe = function(x) {
+		$scope.myOrderBy = x;
+	}
+});
