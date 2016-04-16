9d08ef335c3061d4ad021a0b213d6c4fbd636ce2
AriaCundick <aria.cundick@gmail.com>
2016-04-16 17:39:19 -0500

    c

--- namesController.js
+++ namesController.js
@@ -11,7 +11,7 @@ angular.module('myApp', []).controller('namesCtrl', function($scope) {
 	];
 
 	$scope.enableAdd = function() {
-		if($scope.addName === '')
+		if($scope.addName === "")
 			document.getElementById("addBtn").disabled = true;
 		else
 			document.getElementById("addBtn").disabled = false;
