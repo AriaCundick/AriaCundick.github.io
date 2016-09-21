f5a5e6cf5f4f080520bc8e970fb0224bacb60c2d
AriaCundick <aria.cundick@gmail.com>
2016-09-21 18:27:18 -0500

    r

--- portfolio.js
+++ portfolio.js
@@ -4,15 +4,15 @@ var popup = false;
 
 $(document).ready( function() {
 
-	$("#nav1").css("background-color", "rgba(240, 230, 230, 1)");
+	$("#nav1").css("background-color", "rgba(43, 42, 36, 1)");
 	$("#nav1").mouseover( function() 
 	{
 		//Keep light color on active page
-		$("#nav1").css("background-color", "rgba(240, 230, 230, 1)");
-		$("#nav2").css("background-color", "rgba(240, 230, 230, 0)");
-		$("#nav3").css("background-color", "rgba(240, 230, 230, 0)");
+		$("#nav1").css("background-color", "rgba(43, 42, 36, 1)");
+		$("#nav2").css("background-color", "rgba(43, 42, 36, 0)");
+		$("#nav3").css("background-color", "rgba(43, 42, 36, 0)");
 
-		$("#slide1").fadeIn(200).css("display", "flex");
+		$("#slide1").css("display", "flex");
 		$("#slide2").css("display", "none");
 		$("#slide3").css("display", "none");
 
@@ -21,25 +21,25 @@ $(document).ready( function() {
 	$("#nav2").mouseover( function() 
 	{
 		//Keep light color on active page
-		$("#nav1").css("background-color", "rgba(240, 230, 230, 0)");
-		$("#nav2").css("background-color", "rgba(240, 230, 230, 1)");
-		$("#nav3").css("background-color", "rgba(240, 230, 230, 0)");
+		$("#nav1").css("background-color", "rgba(43, 42, 36, 0)");
+		$("#nav2").css("background-color", "rgba(43, 42, 36, 1)");
+		$("#nav3").css("background-color", "rgba(43, 42, 36, 0)");
 
 		$("#slide1").css("display", "none");
-		$("#slide2").fadeIn(200).css("display", "block");
+		$("#slide2").css("display", "block");
 		$("#slide3").css("display", "none");
 	});
 
 	$("#nav3").mouseover( function() 
 	{
 		//Keep light color on active page
-		$("#nav1").css("background-color", "rgba(240, 230, 230, 0)");
-		$("#nav2").css("background-color", "rgba(240, 230, 230, 0)");
-		$("#nav3").css("background-color", "rgba(240, 230, 230, 1)");
+		$("#nav1").css("background-color", "rgba(43, 42, 36, 0)");
+		$("#nav2").css("background-color", "rgba(43, 42, 36, 0)");
+		$("#nav3").css("background-color", "rgba(43, 42, 36, 1)");
 
 		$("#slide1").css("display", "none");
 		$("#slide2").css("display", "none");
-		$("#slide3").fadeIn(200).css("display", "block");
+		$("#slide3").css("display", "block");
 	});
 
 	$(".btn").on("click", function(event) {
